f9e2b6bed7dd31571aa1aa8a6c5f6e531f8d3470
D. T. Nguyen <idahousa@users.noreply.github.com>
2018-03-22 17:30:11 +0900

    Update LinearSVC_2Classes.py

--- LinearSVC_2Classes.py
+++ LinearSVC_2Classes.py
@@ -1,9 +1,16 @@
+#Date-of-code: 2018-03-22
+#Author: datnt
+#Descriptions:
+#Example of using LinearSVC for linear support vector machine classification
+#Using LinearSVC for 2-classes classification problem
+#Note: Data must be normalized before using (such as the use of StandardScaler())
+#      Please use loss = 'hinge' for better loss function.
 import numpy as np
 from sklearn import datasets
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
-
+#Load data and training svm model
 iris = datasets.load_iris()
 data  = iris.data[:,2:]
 label = (iris.target==2).astype(np.float64)
@@ -14,3 +21,4 @@ print('Distance to hyperplance is: {}'.format(clf.decision_function([[0.5, 1.7]]
 print('Class label is: {}'.format(clf.predict([[0.5, 1.7]])))
 print('Training accuracy is: {}'.format(clf.score(data,label)))
 print(clf)
+#End of code
